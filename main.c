4d81216a494e704596bb465b37971db43936303c
pradhanashis2k03 <100114532+pradhanashis2k03@users.noreply.github.com>
2024-11-05 21:05:52 +0530

    Update main.c

--- main.c
+++ main.c
@@ -13,9 +13,25 @@ uint32_t TxData = 0x00;
 uint32_t sine_digitized = 0;
 int main(void)
 {
-    INIT_GPIO_PORTF_REGISTERS();
-    INIT_GPIO_PORTB_REGISTERS();
-    INIT_GPIO_PORTA_REGISTERS();
+    SYSCTL_RCGCGPIO_R |= 0x20;
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;     /* unlock commit register */
+    GPIO_PORTF_CR_R = 0x1F;             /* make PORTF configurable */
+    GPIO_PORTF_DEN_R = 0x1F;            /* set PORTF pins 4 : 0 pins */
+    GPIO_PORTF_DIR_R = 0x0E;            /*  */
+    GPIO_PORTF_PUR_R = 0x11;            /* PORTF0 and PORTF4 are pulled up */
+    SYSCTL_RCGCGPIO_R |= 0x02;
+    GPIO_PORTB_LOCK_R = 0x4C4F434B;     /* unlock commit register */
+    GPIO_PORTB_DEN_R = 0x0C;
+    GPIO_PORTB_AFSEL_R = 0x0C;
+    GPIO_PORTB_ODR_R = 0x08;
+    GPIO_PORTB_PCTL_R = 0x3300;
+    GPIO_PORTB_PUR_R = 0x0C;
+    SYSCTL_RCGCGPIO_R |= 0x01;
+    GPIO_PORTA_LOCK_R = 0x4C4F434B;     /* unlock commit register */
+    GPIO_PORTA_DEN_R = 0xC0;
+    GPIO_PORTA_AFSEL_R = 0xC0;
+    GPIO_PORTA_ODR_R = 0x80;
+    GPIO_PORTA_PCTL_R = 0x33000000;
 
     SYSCTL_RCGCI2C_R = 0x03;
     NVIC_EN1_R = 0x00000020; // NVIC enable for I2C module 1
@@ -43,41 +59,6 @@ int main(void)
 
     }
     return 0;
-}
-
-void INIT_GPIO_PORTF_REGISTERS(){
-
-    SYSCTL_RCGCGPIO_R |= 0x20;
-    GPIO_PORTF_LOCK_R = 0x4C4F434B;     /* unlock commit register */
-    GPIO_PORTF_CR_R = 0x1F;             /* make PORTF configurable */
-    GPIO_PORTF_DEN_R = 0x1F;            /* set PORTF pins 4 : 0 pins */
-    GPIO_PORTF_DIR_R = 0x0E;            /*  */
-    GPIO_PORTF_PUR_R = 0x11;            /* PORTF0 and PORTF4 are pulled up */
-
-}
-
-void INIT_GPIO_PORTB_REGISTERS(){
-
-    SYSCTL_RCGCGPIO_R |= 0x02;
-    GPIO_PORTB_LOCK_R = 0x4C4F434B;     /* unlock commit register */
-    GPIO_PORTB_DEN_R = 0x0C;
-    GPIO_PORTB_AFSEL_R = 0x0C;
-    GPIO_PORTB_ODR_R = 0x08;
-    GPIO_PORTB_PCTL_R = 0x3300;
-    GPIO_PORTB_PUR_R = 0x0C;
-    //GPIODIR
-}
-
-void INIT_GPIO_PORTA_REGISTERS(){
-
-    SYSCTL_RCGCGPIO_R |= 0x01;
-    GPIO_PORTA_LOCK_R = 0x4C4F434B;     /* unlock commit register */
-    GPIO_PORTA_DEN_R = 0xC0;
-    GPIO_PORTA_AFSEL_R = 0xC0;
-    GPIO_PORTA_ODR_R = 0x80;
-    GPIO_PORTA_PCTL_R = 0x33000000;
-//    GPIO_PORTA_PUR_R = 0xC0;
-    //GPIODIR
 }
 
 void TxDAC(uint8_t Slave_Addr, int n_bytes, uint32_t data){
@@ -134,7 +115,6 @@ void I2C_SLAVE_HANDLER(){
         GPIO_PORTF_DATA_R = 0x08;
     }
     I2C1_SICR_R = 0x01;
-//    I2C1_SIMR_R = 0x01; // interrupt enabled for data rx at slave
 }
 
 void ISR(){
@@ -144,5 +124,4 @@ void ISR(){
     if(TxData >= 4096){
         TxData = 0;
     }
-//    NVIC_ST_CURRENT_R = 16000*500;
 }
